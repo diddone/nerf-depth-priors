9eac3bc7dc28d39cb7d618eb08b3beda401c75cf
diddone <dmmpozdeev@gmail.com>
2024-02-02 21:01:13 +0300

    change init in new_model

--- new_model/__init__.py
+++ new_model/__init__.py
@@ -1,3 +1,5 @@
 from .nerf import VanillaNeRFRadianceField
 from .mlp import  NerfMLP, get_embedder, get_rays, sample_pdf, img2mse, mse2psnr, to8b, to16b, \
                   precompute_quadratic_samples, compute_depth_loss, select_coordinates
+
+from .rendering import render_image_with_occgrid
