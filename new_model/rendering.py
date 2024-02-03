5cf005ab8095bdbbe55d594d20cc435e6d4c0be9
diddone <dmmpozdeev@gmail.com>
2024-02-03 19:21:53 +0300

    typo

--- new_model/rendering.py
+++ new_model/rendering.py
@@ -31,6 +31,7 @@ def render_image_with_occgrid(
 ):
     """Render the pixels of an image."""
 
+    rays_shape = all_rays_o.shape
     num_rays = all_rays_o.shape[0]
     results = []
     chunk = (
@@ -38,7 +39,7 @@ def render_image_with_occgrid(
         if radiance_field.training
         else test_chunk_size
     )
-    rays_shape = rays_o.shape
+
     # run netwrok chunk by chunk to avoid OOM
     for i in range(0, num_rays, chunk):
         # chunk_rays = namedtuple_map(lambda r: r[i : i + chunk], rays)
