a5dbbd38b9637007dfd368a20ddecb77b26ff509
diddone <dmmpozdeev@gmail.com>
2024-07-11 13:42:13 +0200

    quick fix

--- new_model/__init__.py
+++ new_model/__init__.py
@@ -3,4 +3,4 @@ from .mlp import  NerfMLP, get_embedder, get_rays, sample_pdf, img2mse, mse2psnr
                   precompute_quadratic_samples, compute_depth_loss, select_coordinates
 
 from .rendering import render_image_with_occgrid
-from .estimators import OccGridEstim, PropNetEstim
+# from .estimators import OccGridEstim, PropNetEstim
